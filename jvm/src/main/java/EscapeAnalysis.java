aa6fba86f34b1ec0aad2ff4a95ac1f27ab730a50
lihao <lihaowhu@gmail.com>
2016-03-16 15:56:54 +0800

    add escape analysis

---
+++ jvm/src/main/java/EscapeAnalysis.java
@@ -0,0 +1,23 @@
+/**
+ * Created by mandy on 2016/3/16.
+ */
+public class EscapeAnalysis {
+    private static class Foo {
+        private int x;
+        private static int counter;
+
+        public Foo(int k) {
+            x = (++k);
+        }
+    }
+    public static void main(String[] args){
+        long start = System.nanoTime();
+       int k;
+        for (int i = 0; i < 1000 * 1000 * 10; ++i) {
+            k = i +2;
+            Foo foo = new Foo(k);
+        }
+        long end = System.nanoTime();
+        System.out.println("Time cost is " + (end - start));
+    }
+}
