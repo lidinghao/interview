74cde16727e545771f908e07c1bd402a2a8a9048
lihao <lihaowhu@gmail.com>
2016-05-05 21:37:54 +0800

    add solution for single number 3

--- algorithms/src/main/java/BitManipulation/SingleNumber.java
+++ algorithms/src/main/java/BitManipulation/SingleNumber.java
@@ -25,11 +25,33 @@ public class SingleNumber {
         }
         return one;
     }
+
+    public int[] singleNumber3(int[] nums) {
+        int result  = 0;
+        for (int num : nums) {
+            result = result ^ num;
+        }
+        int k = 0;
+        for (; k < Integer.SIZE; k++) {
+            if (((result >>> k)&1) == 1) {
+                break;
+            }
+        }
+        int x = 0;
+        for (int num : nums) {
+            if (((num >>> k) & 1) == 1) {
+                x ^= num;
+            }
+        }
+        return new int[]{x, x ^ result};
+    }
     @Test
     public void test() {
         Assert.assertEquals(3, singleNumber(new int[]{1, 2, 3, 2, 1}));
         Assert.assertEquals(2, singleNumber(new int[]{1, 2, 1}));
         Assert.assertEquals(2, singleNumber2(new int[]{1, 2, 1, 1}));
         Assert.assertEquals(5, singleNumber2(new int[]{1, 2, 5, 1, 2, 2, 1}));
+        Assert.assertArrayEquals(new int[]{3,5}, singleNumber3(new int[]{1, 2, 5, 1, 2, 3}));
+        Assert.assertArrayEquals(new int[]{3,1}, singleNumber3(new int[]{1, 2, 5, 2,5, 3}));
     }
 }
